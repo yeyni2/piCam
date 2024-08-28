442b7326e09e5940677702ef558abce421385da8
yeyni2 <liranyeyni1620@gmail.com>
2024-08-29 00:20:25 +0300

    remove app run flask

--- live_feed.py
+++ live_feed.py
@@ -125,7 +125,6 @@ def main():
     t.daemon = True
     t.start()
     socketio.run(app, host='0.0.0.0', port=3000, allow_unsafe_werkzeug=True)
-    # app.run(host='0.0.0.0', port=3000)
 
 
 if __name__ == '__main__':

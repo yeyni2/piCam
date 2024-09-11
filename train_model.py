b32f87f5ae1ebcf747714999c4c769d07637893f
yeyni2 <liranyeyni1620@gmail.com>
2024-09-11 23:25:01 +0200

    added user images

--- train_model.py
+++ train_model.py
@@ -1,47 +1,34 @@
-#! /usr/bin/python
-
+import numpy as np
 from imutils import paths
 import face_recognition
 import pickle
 import cv2
 import os
 
-# our images are located in the dataset folder
 print("[INFO] start processing faces...")
 imagePaths = list(paths.list_images("dataset"))
 
-# initialize the list of known encodings and known names
-knownEncodings = []
-knownNames = []
+dir_path = os.path.dirname(os.path.realpath(__file__))
+encodingsP = os.path.join(dir_path, 'encodings.pickle')
+data = pickle.loads(open(encodingsP, "rb").read())
 
-# loop over the image paths
+knownEncodings = data["encodings"] if data is not None else []
+knownNames = data["names"] if data is not None else []
+
 for (i, imagePath) in enumerate(imagePaths):
-	# extract the person name from the image path
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
+	print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
 	name = imagePath.split(os.path.sep)[-2]
-
-	# load the input image and convert it from RGB (OpenCV ordering)
-	# to dlib ordering (RGB)
 	image = cv2.imread(imagePath)
 	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-	# detect the (x, y)-coordinates of the bounding boxes
-	# corresponding to each face in the input image
-	boxes = face_recognition.face_locations(rgb,
-		model="hog")
-
-	# compute the facial embedding for the face
+	boxes = face_recognition.face_locations(rgb, model="hog")
 	encodings = face_recognition.face_encodings(rgb, boxes)
 
-	# loop over the encodings
-	for encoding in encodings:
-		# add each encoding + name to our set of known names and
-		# encodings
-		knownEncodings.append(encoding)
-		knownNames.append(name)
+	for new_encoding in encodings:
+		if not any(np.array_equal(known_encoding, new_encoding) for known_encoding in knownEncodings):
+			knownEncodings.append(new_encoding)
+			knownNames.append(name)
 
-# dump the facial encodings + names to disk
 print("[INFO] serializing encodings...")
 data = {"encodings": knownEncodings, "names": knownNames}
 f = open("encodings.pickle", "wb")

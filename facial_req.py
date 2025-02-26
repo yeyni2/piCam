101d0c1929b7b622b768fdb72969b6b7a5fca3c4
yeyni2 <liranyeyni1620@gmail.com>
2025-02-26 22:56:12 +0200

    fuck

--- facial_req.py
+++ facial_req.py
@@ -334,63 +334,63 @@ def activate_camera(frame_info=None, show_on_screen=False):
     time_count = time.time()
     last_frame = None
 
-    # try:
-    while True:
-        start_time = time.time()
-        update_data()
+    try:
+        while True:
+            start_time = time.time()
+            update_data()
 
-        frame, boxes, encodings = handle_frame()
-        amount_of_faces = max(amount_of_faces, len(boxes))
-        users = match_existing_faces(encodings, users)
+            frame, boxes, encodings = handle_frame()
+            amount_of_faces = max(amount_of_faces, len(boxes))
+            users = match_existing_faces(encodings, users)
 
-        if show_on_screen or "user_connections" in frame_info and len(frame_info["user_connections"]) > 0:
-            user_connected = True
-        else:
-            user_connected = False
-
-        if len(users) > 0 or expect_face:
-            expect_face = True
-            frames_validate_count += 1
-
-        frame_info["frame_rate"] = get_fps(user_connected, expect_face)
-
-        if user_connected or show_on_screen:
-            frame = draw_box_around_faces(boxes, users, frame)
-
-        frame = add_data_time(frame)
-
-        if frames_validate_count == FRAME_NOTIFICATION_THRESHOLD:
-            if len(users) > 0:
-                notify_relevant_users(seen_users=users, expected_faces_count=amount_of_faces)
-
-            expect_face = False
-            frames_validate_count = 0
-            users = []
-            amount_of_faces = 0
-
-        if show_on_screen:
-            cv2.imshow("Facial Recognition is Running", frame)
-
-        frame_info["frame"] = frame
-
-        iteration_time = time.time() - start_time
-        sleep_time = max(1 / frame_info["frame_rate"] - iteration_time, 0)
-        time.sleep(sleep_time)
-
-        if time.time() - time_count >= 30:
-            frame_info["last_validation"] = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-            time_count = time.time()
-            if last_frame is not None and np.array_equal(last_frame, frame):
-                frame_info["is_frame_stuck"] = True
+            if show_on_screen or "user_connections" in frame_info and len(frame_info["user_connections"]) > 0:
+                user_connected = True
             else:
-                frame_info["is_frame_stuck"] = False
-            last_frame = frame
-    # except Exception as e:
-    #     print("face rec stopped", e)
-    # finally:
-    #     cv2.destroyAllWindows()
-    #     vs.stop()
-    #     print("somthing failed!!!!!!!!!")
+                user_connected = False
+
+            if len(users) > 0 or expect_face:
+                expect_face = True
+                frames_validate_count += 1
+
+            frame_info["frame_rate"] = get_fps(user_connected, expect_face)
+
+            if user_connected or show_on_screen:
+                frame = draw_box_around_faces(boxes, users, frame)
+
+            frame = add_data_time(frame)
+
+            if frames_validate_count == FRAME_NOTIFICATION_THRESHOLD:
+                if len(users) > 0:
+                    notify_relevant_users(seen_users=users, expected_faces_count=amount_of_faces)
+
+                expect_face = False
+                frames_validate_count = 0
+                users = []
+                amount_of_faces = 0
+
+            if show_on_screen:
+                cv2.imshow("Facial Recognition is Running", frame)
+
+            frame_info["frame"] = frame
+
+            iteration_time = time.time() - start_time
+            sleep_time = max(1 / frame_info["frame_rate"] - iteration_time, 0)
+            time.sleep(sleep_time)
+
+            if time.time() - time_count >= 30:
+                frame_info["last_validation"] = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+                time_count = time.time()
+                if last_frame is not None and np.array_equal(last_frame, frame):
+                    frame_info["is_frame_stuck"] = True
+                else:
+                    frame_info["is_frame_stuck"] = False
+                last_frame = frame
+    except Exception as e:
+        print("face rec stopped", e)
+    finally:
+        cv2.destroyAllWindows()
+        vs.stop()
+        print("somthing failed!!!!!!!!!")
 
 
 if __name__ == '__main__':
